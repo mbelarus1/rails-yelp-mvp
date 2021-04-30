21643b61827425805a6b776ed08fb6fe340291a8
Nikita Siniauski <nsiniauski@gmail.com>
2021-04-29 21:00:38 -0500

    done

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_28_010509) do
+ActiveRecord::Schema.define(version: 2021_04_28_012624) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2021_04_28_010509) do
     t.string "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "restaurant_id"
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "inititating seed..."
+        100.times do
+          Restaurant.create(
+          name: Faker::Restaurant.name,
+          address: Faker::Address.full_address,
+          category: ["chinese", "italian", "japanese", "french", "belgian"].sample
+          )
+          puts "restaurant... created"
+        end
